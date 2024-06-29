a229a78b1d7e7383049760947710c6417acacf18
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-06-29 09:17:54 +0200

    feat: create action added

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateEventDto } from './dto/create-event.dto';
+import { PrismaService } from './prisma.service';
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly _prismaService: PrismaService) {}
+
+  async createEvent(createEventDto: CreateEventDto) {
+    const { userId, eventId, action } = createEventDto;
+
+    return this._prismaService.googleCalendarEvent.create({
+      data: {
+        userId,
+        eventId,
+        action,
+      },
+    });
   }
 }
